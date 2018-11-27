b49555a8130093c567a2474176119b59dce25cf7
edfeld <edfeld1@gmail.com>
2018-11-26 20:57:28 -0800

    pushing models - users and logins

--- models/logins.js
+++ models/logins.js
@@ -1,21 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
   var logins = sequelize.define("logins", {
-    password_hash: DataTypes.STRING,
-    password_salt: DataTypes.STRING
+    passwordHash: DataTypes.STRING,
+    passwordSalt: DataTypes.STRING
   });
   return logins;
 };
 
-/* 
-  -- logins
-CREATE TABLE logins
-(
-	id int NOT NULL AUTO_INCREMENT,
-	related_user_id int NOT NULL,
-    password_hash varchar(300) NOT NULL, -- requires review
-    password_salt varchar(300) NOT NULL, -- requires review
-	PRIMARY KEY (id)
-    -- FOREIGN KEY (related_user_id) REFERENCES memberships(related_user_id)
-);
-  
-  */
+//   -- logins
+// CREATE TABLE logins
+// (
+// 	id int NOT NULL AUTO_INCREMENT,
+// 	related_user_id int NOT NULL,
+//     password_hash varchar(300) NOT NULL, -- requires review
+//     password_salt varchar(300) NOT NULL, -- requires review
+// 	PRIMARY KEY (id)
+//     -- FOREIGN KEY (related_user_id) REFERENCES memberships(related_user_id)
+// );
