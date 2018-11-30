934cae9d82db7bff3c0c2efd3b2d79f1f810756f
edfeld <edfeld1@gmail.com>
2018-11-29 19:12:42 -0800

    updates to db tables to improve FK connections.  implementing some api routes: get users, user w/logins, lookup user by email, inventory+users

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,36 @@
--- INSERT INTO users (username, first_name, last_name, street_address1, city, _state, zip, email_address, phone1) 
--- VALUES ('jaxdot', 'jacqueline', 'gaherity', '15771 oxford way', 'sandwich', 'ma', '98012', 'jacquelne.gaherity@UW.edu', '508-888-9755');
+/* */
+
+use project2_db;
+-- drop database if exists project2_db;
+   create database if not exists project2_db;
+-- DROP TABLE IF EXISTS inventory;
+-- DROP TABLE  IF EXISTS users;
+-- DROP TABLE IF EXISTS logins
+-- DROP TABLE IF EXISTS purchase_orders;
+
+INSERT INTO users (username, first_name, last_name, street_address1, city, _state, zip, email_address, phone1) 
+VALUES ('jaxdot', 'jacqueline', 'gaherity', '15771 oxford way', 'sandwich', 'ma', '98012', 'jacquelne.gaherity@UW.edu', '508-888-9755');
+
+INSERT INTO users (username, first_name, last_name, street_address1, city, _state, zip, email_address, phone1) 
+VALUES ('edfeld', 'ed', 'einfeld', '3833 238th st SW', 'brier', 'WA', '98036', 'ed.einfeld@UW.edu', '425-780-0713');
+
+INSERT INTO users (username, first_name, last_name, street_address1, city, _state, zip, email_address, phone1) 
+VALUES ('bwoods', 'bob', 'woods', '777 238th st SW', 'charlotte', 'va', '90001', 'bob.woods@UW.edu', '428-780-0715');
+
+INSERT INTO users (username, first_name, last_name, street_address1, city, _state, zip, email_address, phone1) 
+VALUES ('bwoods', 'bob', 'woods', '777 238th st SW', 'charlotte', 'va', '90001', 'bob.woods@UW.edu', '428-780-0715');
+
+
+USE project2_db;
+insert into inventory (product_name, description, requested_sale_price, commision_rate, consignor_paid_checkbox, userId) values ('grass trimmer', 'husquevarna', '49.00', '.02', '0', '1');
+
+insert into inventory (product_name, description, requested_sale_price, commision_rate, consignor_paid_checkbox, userId) VALUES ('electric trimmer', 'echo brand', '75.00', '.02', '0', '2');
+
+INSERT INTO inventory (product_name, description, requested_sale_price, commision_rate, consignor_paid_checkbox, userId) VALUES ('bicycle', 'schwinn 10 speed', '149.00', '.02', '0', '3');
+
+
+use project2_db;
+INSERT INTO logins (passwordHash, passwordSalt, userId) VALUES ('Hashone', 'saltOne', '1');
+INSERT INTO logins (passwordHash, passwordSalt, userId) VALUES ('Hastwo', 'salttwo', '2');
+INSERT INTO logins (`passwordHash`, `passwordSalt`, `userId`) VALUES ('HasThree', 'Saltthree', '3');
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,6 +8,43 @@ module.exports = function(app) {
     });
   });
 
+  // Get all inventory
+  app.get("/api/inventory", function(req, res) {
+    db.inventory.findAll({}).then(function(dbInventories) {
+      res.json(dbInventories);
+    });
+  });
+
+  // Get all inventory with Users
+  app.get("/api/inventory_users", function(req, res) {
+    db.inventory.findAll({ include: [db.users] }).then(function(dbInvPlusUser) {
+      res.json(dbInvPlusUser);
+    });
+  });
+
+  // Get all users
+  app.get("/api/users", function(req, res) {
+    db.users.findAll().then(function(dbUsers) {
+      res.json(dbUsers);
+    });
+  });
+
+  // Get all users with logins
+  app.get("/api/users_logins", function(req, res) {
+    db.users.findAll({ include: [db.logins] }).then(function(dbUsers) {
+      res.json(dbUsers);
+    });
+  });
+
+  // Get a user with login where user email = :email
+  app.get("/api/user_login/:email", function(req, res) {
+    db.users
+      .findAll({ where: { email_address: req.params.email } })
+      .then(function(dbUsers) {
+        res.json(dbUsers);
+      });
+  });
+
   // Get all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
@@ -16,11 +53,11 @@ module.exports = function(app) {
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.post("/api/examples", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
   // Delete an example by id
   // app.delete("/api/examples/:id", function(req, res) {
