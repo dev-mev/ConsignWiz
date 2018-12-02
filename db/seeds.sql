8e6088a81c001589374ea03ea91c2effc942eff7
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-12-02 13:06:26 -0800

    set up authorization for viewing consignment status- consolidated routes

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,17 +9,17 @@
 -- DROP TABLE IF EXISTS purchase_orders;
 
 use project2_db;
-INSERT INTO users (username, first_name, last_name, street_address1, city, _state, zip, email_address, phone1, passwordHashSalt) 
-VALUES ('jaxdot', 'jacqueline', 'gaherity', '15771 oxford way', 'sandwich', 'ma', '98012', 'jacquelne.gaherity@UW.edu', '508-888-9755', '$2b$10$T6OI5x67dgj.mtujeJJulupH5G47xr5ZrVnnFKSIZxi.cjFs3/hty'),
-               ('edfeld', 'ed', 'einfeld', '3833 238th st SW', 'brier', 'WA', '98036', 'ed.einfeld@UW.edu', '425-780-0713', '$2b$10$j9ncLVxmfGN1bC21S0u4cOjQXskhuRVPeGYVjt5iLkXEss.gPpIAa'),
-			   ('mev', 'molly', 'vinson', '800 238th st SW', 'port orchard', 'WA', '98732', 'mollyV@UW.edu', '477-780-0733', '$2b$10$v/4t8KWidAgu00Gohl/rquFFP9D9T./oJsmt6ezljV/auBGcqbj3q'),
-               ('bwoods', 'bob', 'woods', '777 238th st SW', 'charlotte', 'va', '90001', 'bob.woods@UW.edu', '428-780-0715', '$2b$10$YNBwxg413hPJeuIXTck/suUGO9n82GsMwjWK5lvY2/pCaqAJP.x/a');
+INSERT INTO users (username, first_name, last_name, street_address1, city, _state, zip, email_address, phone1, passwordHashSalt, userType) 
+VALUES ('jaxdot', 'jacqueline', 'gaherity', '15771 oxford way', 'sandwich', 'ma', '98012', 'jacquelne.gaherity@UW.edu', '508-888-9755', '$2b$10$T6OI5x67dgj.mtujeJJulupH5G47xr5ZrVnnFKSIZxi.cjFs3/hty', 'employee'),
+               ('edfeld', 'ed', 'einfeld', '3833 238th st SW', 'brier', 'WA', '98036', 'ed.einfeld@UW.edu', '425-780-0713', '$2b$10$j9ncLVxmfGN1bC21S0u4cOjQXskhuRVPeGYVjt5iLkXEss.gPpIAa', 'employee'),
+			   ('mev', 'molly', 'vinson', '800 238th st SW', 'port orchard', 'WA', '98732', 'mollyV@UW.edu', '477-780-0733', '$2b$10$v/4t8KWidAgu00Gohl/rquFFP9D9T./oJsmt6ezljV/auBGcqbj3q', 'consignor'),
+               ('bwoods', 'bob', 'woods', '777 238th st SW', 'charlotte', 'va', '90001', 'bob.woods@UW.edu', '428-780-0715', '$2b$10$YNBwxg413hPJeuIXTck/suUGO9n82GsMwjWK5lvY2/pCaqAJP.x/a', 'consignor');
 
 
 USE project2_db;
-insert into inventory (product_name, description, requested_sale_price, commision_rate, consignor_paid_checkbox, userId) 
-values ('grass trimmer', 'husquevarna', '49.00', '.02', '0', '1'),
-            ('electric trimmer', 'echo brand', '75.00', '.02', '0', '2'),
-            ('bicycle', 'schwinn 10 speed', '149.00', '.02', '0', '3');
+insert into inventory (product_name, description, received_date, requested_sale_price, sold_at_price, commission_rate, sold_date, consignor_paid_checkbox, userId) 
+values ('grass trimmer', 'husquevarna', '2018/11/01', '49.00', '49.00', '.4', '2018/12/01', '0', '4'),
+            ('electric trimmer', 'echo brand', '2018/11/01', '75.00', null, '.4', null, '0', '4'),
+            ('bicycle', 'schwinn 10 speed', '2018/11/01', '149.00', '125.00','.4', '2018/11/25', '0', '4');
 
 
---
+++ routes/routes.js
@@ -0,0 +1,140 @@
+var db = require("../models");
+var LocalStrategy = require("passport-local").Strategy;
+var bcrypt = require("bcrypt");
+var passport = require("passport");
+var session = require("express-session");
+var express = require("express");
+require("dotenv").config();
+
+module.exports = function(app) {
+  // Passport
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  app.use(express.static("public")); //all static files, look inside the public  folder
+
+  app.use(session({ secret: process.env.secret }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.use(
+    new LocalStrategy(function(username, passwordEntered, done) {
+      db.users
+        .findOne({
+          where: {
+            username: username
+          }
+        })
+        .then(function(userData) {
+          bcrypt.compare(
+            passwordEntered,
+            userData.get("passwordHashSalt"),
+            function(_, isMatch) {
+              if (isMatch) {
+                return done(null, {
+                  username: userData.get("username"),
+                  type: userData.get("userType"),
+                  id: userData.get("id")
+                });
+              } else {
+                return done(null, false);
+              }
+            }
+          );
+        });
+    })
+  );
+
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  function verifyUserType(req, res, next) {
+    if (req.user.type === "consignor") {
+      console.log("accessing consignor view");
+      next();
+    } else {
+      console.log("accessing employee view");
+      next();
+    }
+  }
+
+  app.post(
+    "/login",
+    passport.authenticate("local", { failureRedirect: "/login.html" }),
+    function(req, res) {
+      res.redirect("/index.html");
+    }
+  );
+
+  //Logout route
+  app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/login.html");
+  });
+
+  function isUserAuthenticated(req, res, next) {
+    if (req.user) {
+      console.log("auth succeeded");
+      next();
+    } else {
+      console.log("login please");
+      res.send("You must login!");
+    }
+  }
+
+  // Get all inventory with Users
+  // app.get("/api/inventory_users", function(req, res) {
+  //   db.inventory.findAll({ include: [db.users] }).then(function(dbInvPlusUser) {
+  //     res.json(dbInvPlusUser);
+  //   });
+  // });
+
+  // get all inventory for one consignor
+  app.get("/api/inventory", [isUserAuthenticated, verifyUserType], function(
+    req,
+    res
+  ) {
+    db.inventory
+      .findAll({ where: { userId: req.user.id } })
+      .then(function(dbInventory) {
+        res.json(dbInventory);
+      });
+  });
+
+  // TODO: for testing purposes
+  app.get("/secret", isUserAuthenticated, function(req, res) {
+    res.send("Page to test authentication.");
+    console.log(
+      "secret route accessed by " + req.user.username + " " + req.user.type
+    );
+  });
+
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+
+  // // Get all examples
+  // app.get("/api/examples", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.json(dbExamples);
+  //   });
+  // });
+
+  // Create a new example
+  // app.post("/api/examples", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
+
+  // Delete an example by id
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
+};
