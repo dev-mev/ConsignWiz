de4a2d1d4175188134561b7ca00fb7421067f0bd
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-12-01 19:37:13 -0800

    bcrypt implemented for password hashing and authentication

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-var session = require("express-session");
 
 var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
-// // Middleware
-app.use(session({ secret: "consign" }));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(express.static("public")); //all static files, look inside the public  folder
-
-//Logout route
-app.get("/logout", function(req, res) {
-  req.logout();
-  res.redirect("/login.html");
-});
 
 // Handlebars
 app.engine(
@@ -30,8 +17,8 @@ app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
 require("./routes/loginHtmlRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
