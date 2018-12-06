0a617260a1f9b440f07c1bce7b6ca85512e840fd
dev-mev <40147757+dev-mev@users.noreply.github.com>
2018-12-05 21:49:19 -0800

    fix lint errors

--- public/js/KO.js
+++ public/js/KO.js
@@ -69,23 +69,23 @@ $(document).ready(function() {
     });
   }
 
-  function discoverUserType() {
-    // console.log("begin discoverUserType function");
-    $.ajax({ url: "/api/users", method: "GET" }).then(function(users) {
-      console.log("begin loop in discoverUserType");
-      for (var i = 0; i < users.length; i++) {
-        if (users[i].userType === "consignor") {
-          window.myStudio.ModalStatus("consignment-status");
-          console.log(window.myStudio.ModalStatus());
-          console.log("CONSIGNOR");
-        } else if (users[i].userType === "employee") {
-          console.log("EMPLOYEE");
-        } else {
-          console.log("Cannot get userType");
-        }
-      }
-    });
-  }
+  // function discoverUserType() {
+  //   // console.log("begin discoverUserType function");
+  //   $.ajax({ url: "/api/users", method: "GET" }).then(function(users) {
+  //     console.log("begin loop in discoverUserType");
+  //     for (var i = 0; i < users.length; i++) {
+  //       if (users[i].userType === "consignor") {
+  //         window.myStudio.ModalStatus("consignment-status");
+  //         console.log(window.myStudio.ModalStatus());
+  //         console.log("CONSIGNOR");
+  //       } else if (users[i].userType === "employee") {
+  //         console.log("EMPLOYEE");
+  //       } else {
+  //         console.log("Cannot get userType");
+  //       }
+  //     }
+  //   });
+  // }
 
   //consignor lookup submit
   $(".submit").on("click", function(event) {
